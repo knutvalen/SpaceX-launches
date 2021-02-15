88cb8fa8f82f8db03de71a7ede32a8cbf2fa9d48
Knut Valen <knut.valen@icloud.com>
2021-02-15 09:26:01 +0100

    Update index.js
    add material theme

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
+import { makeStyles, createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+import { blue, green } from "@material-ui/core/colors";
+
+const useStyles = makeStyles((theme) => ({
+    root: { display: "flex" },
+}));
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blue,
+        secondary: green,
+    },
+});
+
 function Index() {
+    const classes = useStyles();
     return (
-        <div>hello world</div>
+        <MuiThemeProvider theme={theme}>
+            <div className={classes.root}>hello world</div>
+        </MuiThemeProvider>
     );
 };
 
