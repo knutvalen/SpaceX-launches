24aa70e3a32da6125a2ee0617b38cdcc880245dd
Knut Valen <knut.valen@icloud.com>
2021-02-15 16:04:02 +0100

    refactor
    refactor the launch data handling into a custom hook called useLaunchInteractor

--- src/ui/home.js
+++ src/ui/home.js
@@ -1,7 +1,6 @@
 import { makeStyles } from "@material-ui/core";
-import axios from "axios";
-import { useEffect, useReducer } from "react";
 import useCountdown from "../hooks/useCountdown";
+import useLaunchInteractor from "../hooks/useLaunchInteractor";
 
 const useStyles = makeStyles((theme) => ({
     toolbar: theme.mixins.toolbar,
@@ -14,65 +13,7 @@ const useStyles = makeStyles((theme) => ({
 function Home() {
     const classes = useStyles();
     const { count } = useCountdown(10);// hard-code 10 seconds countdown for now
-
-    function launchReducer(state, action) {
-        switch (action.type) {
-            case "setNextLaunch": { 
-                return {
-                    ...state,
-                    nextLaunch: action.data,
-                    hasErrored: false,
-                    error: null,
-                };
-            }
-
-            case "setError": { 
-                return {
-                    ...state,
-                    hasErrored: true,
-                    error: action.error
-                };
-            }
-
-            default:
-                return state;
-        }
-    }
-
-    const [
-        { nextLaunch, upcomingLaunches, hasErrored, error },
-        dispatch
-    ] = useReducer(launchReducer, {
-        nextLaunch: null,
-        upcomingLaunches: [],
-        hasErrored: false,
-        error: null,
-    });
-
-    useEffect(() => {
-        const getNextLaunches = async function () {
-            try {
-                let result = await axios.get("https://api.spacexdata.com/v4/launches/upcoming");
-                console.log(result);
-            } catch (e) {
-                console.log(e);
-            }
-        };
-
-        const getNextLaunch = async function () {
-            try {
-                let result = await axios.get("https://api.spacexdata.com/v4/launches/next");
-                console.log(result);
-                dispatch({ type: "setNextLaunch", data: result.data });
-            } catch (e) {
-                console.log(e);
-                dispatch({ type: "setError", error: e });
-            }
-        };
-
-        getNextLaunches();
-        getNextLaunch();
-    }, []);
+    const { nextLaunch, upcomingLaunches, hasErrored, error } = useLaunchInteractor();
 
     return (
         <main className={classes.content}>
