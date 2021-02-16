52670e7ef8547af676b69d9eb0ecddd9cbb615d8
Knut Valen <knut.valen@icloud.com>
2021-02-16 14:51:16 +0100

    format countdown text and refresh counter when counter is below zero

--- src/ui/home.js
+++ src/ui/home.js
@@ -13,40 +13,75 @@ const useStyles = makeStyles((theme) => ({
 function Home() {
     const classes = useStyles();
     const [delay, setDelay] = useState();
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState();
+    const [countdown, setCountdown] = useState();
     const callbackRef = useRef();
+    let countdownTimerID;
 
-    useEffect(() => {
+    function refresh() {
         new Promise(resolve => {
-            let result = axios.get("https://api.spacexdata.com/v4/launches/next");
+            const result = axios.get("https://api.spacexdata.com/v4/launches/next");
             resolve(result);
         }).then(nextLaunch => {
-            console.log(nextLaunch.data.date_unix);
-            setDelay(1000);
-            setCount(10);//TODO: use nextLaunch.data.date_unix to calculate the diff in seconds
+            if (nextLaunch.data.date_utc) {
+                setDelay(1000);
+                const now = new Date().getTime();
+                const nextLaunchTime = new Date(nextLaunch.data.date_utc).getTime();
+                const diffSeconds = Math.floor((nextLaunchTime - now) / 1000);
+                setCount(diffSeconds);
+            } else {
+                setCount(null);
+            }
         });
-    }, []);
+    }
 
     useEffect(() => {
-        callbackRef.current = () => setCount(count - 1);
-    }, [count]);
+        refresh();
+    }, []);
 
     useEffect(() => {
         function tick() {
             callbackRef.current();
         };
 
-        if (delay !== null) {
-            let id = setInterval(tick, delay);
-            return () => clearInterval(id);
+        if (delay) {
+            countdownTimerID = setInterval(tick, delay);
+            return () => clearInterval(countdownTimerID);
         }
     }, [delay]);
+
+    useEffect(() => {
+        callbackRef.current = () => setCount(count - 1);
+
+        function formatCountdown(count) {
+            const days = Math.floor(count / (60 * 60 * 24));
+            const hours = Math.floor((count % (60 * 60 * 24)) / (60 * 60));
+            const minutes = Math.floor((count % (60 * 60)) / 60);
+            const seconds = Math.floor(count % 60);
+            return `${days}d ${hours}t ${minutes}m ${seconds}s`;
+        };
+
+        if (count > 0) {
+            const countdown = formatCountdown(count);
+            console.log(countdown);
+            setCountdown(countdown);
+        }
+
+        if (count === 0) {
+            setCountdown("Liftoff!");
+        }
+
+        if (count < 0) {
+            clearInterval(countdownTimerID);
+            refresh();
+        }
+    }, [count]);
 
     return (
         <main className={classes.content}>
             <div className={classes.toolbar} />
-            {count && count > 0 && (
-                <div>T minus {count}</div>
+            {countdown && (
+                <div>{countdown}</div>
             )}
         </main>
     );
