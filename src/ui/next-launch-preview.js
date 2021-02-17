b23486c3e14d7e96a007698b7d5f7e9c64c86b3c
Knut Valen <knut.valen@icloud.com>
2021-02-17 09:07:00 +0100

    update UI of NextLaunchPreview
    make NextLaunchPreview look like a Material card and add an action button to be able to navigate to the details page

--- src/ui/next-launch-preview.js
+++ src/ui/next-launch-preview.js
@@ -1,12 +1,32 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, Typography } from "@material-ui/core";
+
 export default function NextLaunchPreview({ countdown, launch }) {
+
+    function onDetailsClick() {
+        //TODO
+    }
+
     return (
-        <div>
-            {countdown && (
-                <div>{countdown}</div>
-            )}
-            {launch && (
-                <div>{launch.name}</div>
-            )}
-        </div>
+        <Card>
+            <CardActionArea onClick={() => onDetailsClick()}>
+                <CardContent>
+                    {countdown && (
+                        <Typography variant="h1" align="center">
+                            {countdown}
+                        </Typography>
+                    )}
+                    {launch && (
+                        <Typography variant="subtitle1">
+                            {launch.name}
+                        </Typography>
+                    )}
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button color="secondary" onClick={() => onDetailsClick()}>
+                    Details
+                </Button>
+            </CardActions>
+        </Card>
     );
 };
