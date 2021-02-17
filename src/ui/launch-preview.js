eb0955bc63918a73bc3461dee2916ad8cdcfdc75
Knut Valen <knut.valen@icloud.com>
2021-02-17 20:34:10 +0100

    Update launch-preview.js

--- src/ui/launch-preview.js
+++ src/ui/launch-preview.js
@@ -1,7 +1,41 @@
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    List, ListItem,
+    ListItemText
+} from "@material-ui/core";
+import { useRouter } from "next/router";
+
 export default function LaunchPreview({ launch }) {
+    const router = useRouter();
+
+    function onDetailsClick() {
+        router.push({
+            pathname: '/details',
+            query: {
+                id: launch.id
+            },
+        })
+    }
+
     return (
-        <div>
-            {launch.name}
-        </div>
+        <Card>
+            <CardContent>
+                {launch && (
+                    <List>
+                        <ListItem>
+                            <ListItemText primary="Name" secondary={launch.name} />
+                        </ListItem>
+                    </List>
+                )}
+            </CardContent>
+            <CardActions>
+                <Button color="secondary" onClick={() => onDetailsClick()}>
+                    Details
+                </Button>
+            </CardActions>
+        </Card>
     );
 };
