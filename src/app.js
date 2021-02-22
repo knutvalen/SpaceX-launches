18292fc8b2872281ac56e9a54c3aa5e16af32529
Knut Valen <knut.valen@icloud.com>
2021-02-22 14:33:31 +0100

    use ThemeProvider from styled components

--- src/app.js
+++ src/app.js
@@ -1,59 +1,50 @@
-import { makeStyles, createMuiTheme, MuiThemeProvider, CssBaseline } from "@material-ui/core";
-import { blue, green } from "@material-ui/core/colors";
 import { GlobalProvider } from "./global-state";
+import styled, { ThemeProvider } from "styled-components";
 import Head from "./ui/head";
 import Home from "./ui/home";
 import Launch from "./ui/launch";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-}));
+const Root = styled.div`
+  flex-grow: 1;
+`;
 
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: blue[500],
-        },
-        secondary: {
-            main: green[500],
-        }
-    },
-});
+const theme = {
+  colors: {
+    primary: "#2196f3",
+    secondary: "#4caf50",
+  },
+};
 
 const layoutToShow = (layoutName) => {
-    switch (layoutName) {
-        case "home":
-            return <Home />;
+  switch (layoutName) {
+    case "home":
+      return <Home />;
 
-        case "details":
-            return <Launch />;
+    case "details":
+      return <Launch />;
 
-        default:
-            return null;
-    }
+    default:
+      return null;
+  }
 };
 
 export default function App({ layoutName }) {
-    const classes = useStyles();
-    const layout = layoutToShow(layoutName);
+  const layout = layoutToShow(layoutName);
 
-    return (
-        <GlobalProvider>
-            <MuiThemeProvider theme={theme}>
-                <div className={classes.root}>
-                    <CssBaseline />
-                    {layout ? (
-                        <div>
-                            <Head />
-                            {layout}
-                        </div>
-                    ) :
-                        <div>Not found</div>
-                    }
-                </div>
-            </MuiThemeProvider>
-        </GlobalProvider>
-    );
+  return (
+    <GlobalProvider>
+      <ThemeProvider theme={theme}>
+        <Root>
+          {layout ? (
+            <div>
+              <Head />
+              {layout}
+            </div>
+          ) :
+            <div>Not found</div>
+          }
+        </Root>
+      </ThemeProvider>
+    </GlobalProvider>
+  );
 };
